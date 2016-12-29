1662d0bc4cd75c5a2cfd677b1d200b16e0acf191
Leigh Murray <leighmurray@2wattend.com>
2016-12-30 01:03:34 +1100

    initial commit

---
+++ app.php
@@ -0,0 +1,41 @@
+<?php
+	
+	require_once(__DIR__ . "/vendor/autoload.php");
+	
+	use Symfony\Component\HttpFoundation\Request;
+	
+	$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+	$twig = new Twig_Environment($loader, array(
+	    'cache' => NULL,
+	));	
+
+	$request = Request::createFromGlobals();
+	
+	if ($request->query->has('url') && $request->query->get('url'))
+	{
+		$externalUrl = $request->query->get('url');
+		$mercuryUrl = 'https://mercury.postlight.com/parser?url=';
+		$client = new GuzzleHttp\Client();
+		$mercuryResponse = $client->request('GET', sprintf('%s%s', $mercuryUrl, $externalUrl), [
+		    'headers' => [
+				'Content-Type' => 'application/json',
+				'x-api-key' => '7R6VqwASR1ZRjiEJdNPbF4RSkaUZy7zmnGHeEWEs'
+			]
+		]);
+				echo "<pre>";
+		$mercuryBody = $mercuryResponse->getBody();
+		$mercuryJson = json_decode($mercuryBody);
+		echo($mercuryJson->content);
+	} else if ($request->request->has('markdown') && $request->request->get('markdown')) {
+		$markdownText = $request->request->get('markdown');
+		$Parsedown = new Parsedown();
+
+		echo $Parsedown->text($markdownText); # prints: <p>Hello <em>Parsedown</em>!</p>
+		
+	} else {
+		echo $twig->render('index.twig');
+		exit();
+	}
+	
+
+?>
